be1d7bac1f42800a928687a1fb8606d41a8287ec
Jozsef Kozma <joco@bern.local>
2013-06-20 02:25:27 +0200

    changed prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -79,7 +79,7 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}$PWD%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)$(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$USERNAME@$HOST $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
