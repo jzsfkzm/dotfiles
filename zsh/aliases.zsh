c39182052438ea9f3395b3845692bbfa762ffbe5
József Kozma <jozsef.kozma@gmail.com>
2020-12-26 17:22:36 +0100

    feat(aliases): yarn-up is added as an alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
 alias npm-up="npm-check -E -u"
 alias cal3="gcal -K -s 1 -q HU ."
+alias yarn-up="yarn-check -E -u"
